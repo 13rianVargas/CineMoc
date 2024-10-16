8c0fa173f62e06ebc8e9eb3d3d8fe428fffd2ee3
13rian Vargas <bsvargasc13@gmail.com>
2024-10-16 10:37:43 -0500

    IN DEV - Clase Controlador
    - Reviso el error del if en el case 2 del case 1.
    - Creo métodos para crear cada objeto (nuevaSucursal, etc...).
    - Creo método crearEjemplos generado por IA.

--- src/co/edu/konradlorenz/controller/Controlador.java
+++ src/co/edu/konradlorenz/controller/Controlador.java
@@ -1,5 +1,6 @@
 package co.edu.konradlorenz.controller;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 import co.edu.konradlorenz.model.*;
@@ -22,9 +23,8 @@ public class Controlador {
     public void run() {
         //PASOS:
 
-        // Crear datos base
-        crearSucursales();
-        crearPeliculas();
+        // Crear ejemplos base
+            crearEjemplos();
         //*/
 
         //1.
@@ -43,25 +43,31 @@ public class Controlador {
                     case 2:
 
                         Vista.mostrarPeliculas(listaPeliculasGlobales);
-                        String nombrePelicula = Vista.pedirString("  >> Ingrese el nombre de la película: ");//notCamel
+                        String nombrePelicula = Vista.pedirString("  >> Ingrese el nombre de la película: ");//se puede implementar equalsIgnoreCase en el futuro
                         
                         ArrayList<Sucursal> sucursalesTemp = new ArrayList<>();
                         
                         sucursalesTemp = recorrerSucursales(listaSucursales, nombrePelicula);
                         
-                        //mostrar sucursales donde está la peli con fecha
+                        //TODO: mostrar sucursales donde está la peli con fecha
 
                         for (int i = 0; i < sucursalesTemp.size() ; i++) {
                             for (int j = 0; j < sucursalesTemp.size() ; j++) {
+                                
+                                ///FIXME: Error en el if porque no hay funciones creadas, entonces agregué método crearFunciones() al final
                                 if(sucursalesTemp.get(i).getListaFunciones().get(j).getObjPelicula().getTituloOriginal().equals(nombrePelicula)){
-                                    Vista.mostrarMensaje(objFuncion.toString());
+                                    String imprimirFuncion = sucursalesTemp.get(i).getListaFunciones().toString();
+                                    Vista.mostrarMensaje(imprimirFuncion);
                                 }
+                                //*/
+
+                                Vista.mostrarMensaje("Se supone que ya imprimió las funciones"); //TODO: Borrar después
                             }
                         }
 
                         //peliSelec = seleccionarPelicula();
                         
-                        sucursalSelec = seleccionarSucursal(sucursalesTemp);
+                        //sucursalSelec = seleccionarSucursal(sucursalesTemp);
                     
                         break;
                     default:
@@ -75,10 +81,19 @@ public class Controlador {
                 Vista.mostrarSucursales(listaSucursales);
 
                 break;
+            case 3:
+                
+                break;
+            case 0:
+                
+                break;
             default:
+                
                 break;
         }
+        //switch
     }
+    //run
 
     public Sucursal seleccionarSucursal(ArrayList<Sucursal> lista){
         
@@ -120,8 +135,8 @@ public class Controlador {
 
         ArrayList<Sucursal> sucursalesTemp = new ArrayList<>();
 
-        for (int i = 0 ; i < listaSucursales.size() ; i++) {
-            for (int j = 0 ; j < listaSucursales.size() ; j++) {
+        for (int i = 0 ; i < listaSucursales.get(i).getObjCartelera().getListaPeliculas().size() ; i++) {
+            for (int j = 0 ; j < listaSucursales.get(i).getObjCartelera().getListaPeliculas().size() ; j++) {
                 if(listaSucursales.get(i).getObjCartelera().getListaPeliculas().get(j).getTituloOriginal().equals(tituloPeliculaSelec)){
                     sucursalesTemp.add(listaSucursales.get(i));
                 }
@@ -130,88 +145,36 @@ public class Controlador {
 
         return sucursalesTemp;
     }
+    //recorrerSucursales
 
-    public void crearSucursales(){
+    public Sucursal nuevaSucursal(String nombre, String direccion, String telefono, String descripcion, double descuento,
+    Cartelera objCartelera, ArrayList<SalaCine> listaSalasCine, ArrayList<Funcion> listaFunciones){
+        
+        Sucursal nuevaSucursal = new Sucursal(nombre, direccion, telefono, descripcion, descuento, objCartelera, listaSalasCine, listaFunciones);
 
-        listaSucursales.add(new Sucursal(
-            "CineMoc Star",                
-            "Calle 123",                
-            "3001234567",               
-            "Gran cine con múltiples salas",
-            10.5,                       
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()            
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Max", 
-            "Avenida 45", 
-            "3007654321", 
-            "Cine moderno y cómodo", 
-            15.0, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Embajador", 
-            "Carrera 12", 
-            "3101234567", 
-            "El cine favorito de la ciudad", 
-            12.3, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Mega", 
-            "Calle 77", 
-            "3107654321", 
-            "Experiencia de cine de lujo", 
-            8.0, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Plaza", 
-            "Avenida Central", 
-            "3151234567", 
-            "Cine para toda la familia", 
-            14.2, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Fest", 
-            "Zona Norte", 
-            "3201234567", 
-            "El cine con la mejor tecnología", 
-            9.8, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-        
-        listaSucursales.add(new Sucursal(
-            "CineMoc Plus", 
-            "Zona Sur", 
-            "3211234567", 
-            "Cine con las mejores promociones", 
-            7.5, 
-            new Cartelera(),
-            new ArrayList<>(),
-            new ArrayList<>()
-        ));
-
+        return nuevaSucursal;
     }
-    //crearSucursales
+
+    public SalaCine nuevaSalaCine(String numero, String nombre, int cantSillas){
+        
+        SalaCine nuevaSalaCine = new SalaCine(numero, nombre, cantSillas);
+
+        return nuevaSalaCine;
+    }
+
+    public Cartelera nuevaCartelera(LocalDateTime fecha, ArrayList<Pelicula> listaPeliculas){
+        
+        Cartelera nuevaCartelera = new Cartelera(fecha, listaPeliculas);
+
+        return nuevaCartelera;
+    }
+
+    public Funcion nuevaFuncion(String hora, int dia, Pelicula objPelicula, SalaCine objSalaCine){
+        
+        Funcion nuevaFuncion = new Funcion(hora, dia, objPelicula, objSalaCine);
+
+        return nuevaFuncion;
+    }   
 
     public Pelicula nuevaPelicula(byte id, int duracion, String paisOrigen, String año, String tituloOriginal, String idioma,
     String sinopsis, boolean subtitulos, ArrayList<Realizador> listaRealizadores){
@@ -221,65 +184,68 @@ public class Controlador {
         return nuevaPelicula;
     }
 
-    public void crearPeliculas(){
+    public Director nuevoDirector(String nombre, String nacioalidad, int cantPeliculas){
+        
+        Director nuevoDirector = new Director(nombre, nacioalidad, cantPeliculas);
 
-        Pelicula peli1 = nuevaPelicula((byte) 1, 120, "USA", "2022", "The Last Horizon", "Inglés",
-        "En un futuro post-apocalíptico, un grupo de sobrevivientes lucha por encontrar un nuevo hogar.", 
-        true, new ArrayList<Realizador>());
-            
-        listaPeliculasGlobales.add(peli1);
-
-        for (int i = 0; i < listaSucursales.size(); i++) {
-            listaSucursales.get(i).getObjCartelera().setListaPeliculas(listaPeliculasGlobales);
-        }
-
-        Pelicula peli2 = nuevaPelicula((byte) 2, 95, "Francia", "2020", "L'Art de la Vie", "Francés", "Un pintor solitario encuentra inspiración en una joven con un misterioso pasado.", 
-        true, new ArrayList<Realizador>());
-            
-        listaPeliculasGlobales.add(peli2);
-        for (int i = 0; i < listaSucursales.size(); i++) {
-            listaSucursales.get(i).getObjCartelera().setListaPeliculas(listaPeliculasGlobales);
-        }
-
-        Pelicula peli3 = nuevaPelicula((byte) 3, 110, "Japón", "2019", "Kaze no Tabi", "Japonés",
-        "Una joven viaja a través del Japón rural para descubrir sus raíces familiares.", 
-        false, new ArrayList<Realizador>());
-            
-        listaPeliculasGlobales.add(peli3);
-        for (int i = 0; i < listaSucursales.size(); i++) {
-            listaSucursales.get(i).getObjCartelera().setListaPeliculas(listaPeliculasGlobales);
-        }
-
-        /*/
-        listaPeliculasGlobales.add(new Pelicula((byte) 4, 150, "México", "2021", "El Último Sueño", "Español",
-                "En un México distópico, los sueños se han convertido en una moneda de cambio.", 
-                true, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 5, 130, "India", "2023", "Samsara", "Hindi",
-                "Un monje budista busca el significado de la vida en un viaje espiritual.", 
-                false, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 6, 105, "Reino Unido", "2018", "The Silent Sea", "Inglés",
-                "Un submarino británico se enfrenta a misteriosas fuerzas en las profundidades del océano.", 
-                true, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 7, 140, "Alemania", "2022", "Der Fall", "Alemán",
-                "Un abogado lucha contra un sistema corrupto para exonerar a su cliente inocente.", 
-                true, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 8, 90, "España", "2021", "El Eco de las Montañas", "Español",
-                "Un grupo de amigos redescubre su pasado en una pequeña aldea de montaña.", 
-                true, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 9, 115, "Corea del Sur", "2020", "Yeon", "Coreano",
-                "Una bailarina enfrenta las barreras de una sociedad rígida mientras busca su libertad.", 
-                true, new ArrayList<Realizador>()));
-
-        listaPeliculasGlobales.add(new Pelicula((byte) 10, 125, "Brasil", "2019", "A Floresta Escondida", "Portugués",
-                "Un explorador descubre una tribu olvidada en lo profundo de la selva amazónica.", 
-                false, new ArrayList<Realizador>()));
-        //*/
-
+        return nuevoDirector;
     }
-    //crearPeliculas
+
+    public Actor nuevoActor(String nombre, String nacioalidad, int cantPeliculas){
+        
+        Actor nuevoActor = new Actor(nombre, nacioalidad, cantPeliculas);
+
+        return nuevoActor;
+    }
+
+    public void crearEjemplos() {//AI Generated
+        // Crear Director y Actor
+        Director director1 = nuevoDirector("Christopher Nolan", "Reino Unido", 10);
+        ArrayList<String> personajesActor1 = new ArrayList<>();
+        personajesActor1.add("Batman");
+        Actor actor1 = new Actor("Christian Bale", "Reino Unido", 30, personajesActor1);
+
+        ArrayList<Realizador> realizadores1 = new ArrayList<>();
+        realizadores1.add(director1);
+        realizadores1.add(actor1);
+
+        // Crear Películas
+        Pelicula pelicula1 = nuevaPelicula((byte) 1, 120, "Estados Unidos", "2023", "Inception", "Inglés", "Sueños dentro de sueños", true, realizadores1);
+        Pelicula pelicula2 = nuevaPelicula((byte) 2, 150, "Reino Unido", "2020", "Tenet", "Inglés", "Inversión del tiempo", true, realizadores1);
+        Pelicula pelicula3 = nuevaPelicula((byte) 3, 140, "Estados Unidos", "2019", "Interstellar", "Inglés", "Viaje espacial", true, realizadores1);
+
+        // Añadir películas a la lista global
+        listaPeliculasGlobales.add(pelicula1);
+        listaPeliculasGlobales.add(pelicula2);
+        listaPeliculasGlobales.add(pelicula3);
+
+        // Crear 7 Sucursales
+        for (int i = 1; i <= 7; i++) {
+            // Crear 3 SalaCine por Sucursal
+            ArrayList<SalaCine> listaSalas = new ArrayList<>();
+            listaSalas.add(nuevaSalaCine("Sala 1", "Sala A", 100));
+            listaSalas.add(nuevaSalaCine("Sala 2", "Sala B", 120));
+            listaSalas.add(nuevaSalaCine("Sala 3", "Sala C", 80));
+
+            // Crear Cartelera para cada sucursal con las películas
+            ArrayList<Pelicula> peliculasCartelera = new ArrayList<>();
+            peliculasCartelera.add(pelicula1);
+            peliculasCartelera.add(pelicula2);
+            peliculasCartelera.add(pelicula3);
+            Cartelera cartelera = nuevaCartelera(LocalDateTime.now(), peliculasCartelera);
+
+            // Crear 3 Funciones por sucursal
+            ArrayList<Funcion> listaFunciones = new ArrayList<>();
+            listaFunciones.add(nuevaFuncion("15:00", 1, pelicula1, listaSalas.get(0)));
+            listaFunciones.add(nuevaFuncion("18:00", 1, pelicula2, listaSalas.get(1)));
+            listaFunciones.add(nuevaFuncion("21:00", 1, pelicula3, listaSalas.get(2)));
+
+            // Crear Sucursal con los datos
+            Sucursal nuevaSucursal = nuevaSucursal("Sucursal " + i, "Dirección " + i, "Teléfono " + i, "Descripción " + i, 0.1 * i, cartelera, listaSalas, listaFunciones);
+            listaSucursales.add(nuevaSucursal);
+        }
+        //for
+    }
+    //crearEjemplosCine
 }
+//class
